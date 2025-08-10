0286fe4754b7b6155c49411a5894cb455b606c6d
GraceKayem <11557@holbertonstudents.com>
2025-08-10 19:32:19 +0000

    save

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,12 +12,12 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	int count = 0;
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
+
+	int count = 0;
 
 	if ((tree->left != NULL) != (tree->right != NULL))
 	{
