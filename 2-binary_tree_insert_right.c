15e5a079f612c0d6115dce67cce24e1ae8c0dd8d
GraceKayem <11557@holbertonstudents.com>
2025-08-10 19:09:34 +0000

    save

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -25,7 +25,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
 		newNode->right = parent->right;
 		parent->right->parent = newNode;
