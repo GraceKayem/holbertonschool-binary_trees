f3ce5473c31fa1f053bba80e70417853ba6a6bf1
GraceKayem <11557@holbertonstudents.com>
2025-08-10 19:29:06 +0000

    save

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -18,8 +18,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree->left == NULL && tree->right == NULL)
 	{
-		return (0);
+		return (1);
 	}
 
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (binary_tree_leaves(tree->left) 
+		+ binary_tree_leaves(tree->right));
 }
