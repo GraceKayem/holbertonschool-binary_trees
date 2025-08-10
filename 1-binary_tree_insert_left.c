6265f6fee1aef8aee3a974af9aaffaa27c67bf00
GraceKayem <11557@holbertonstudents.com>
2025-08-10 18:51:49 +0000

    save

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	newNode = binary_tree_node(parent, value);
+	newNode = binary_tree_insert_left(parent, value);
 	if (newNode == NULL)
 	{
 		return (NULL);
