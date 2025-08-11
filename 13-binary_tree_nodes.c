69dcc97aaad65b17b5246484be4d12bfb8d8dfd4
GraceKayem <11557@holbertonstudents.com>
2025-08-11 15:17:39 +0000

    save

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,6 +3,67 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_node - Entry point
+ *
+ * @parent: function that creates a binary tree node
+ * @value: function that creates a binary tree node
+ *
+ * Return: Always 0 (Success)
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return (newNode);
+}
+
+/**
+ * binary_tree_insert_right - Entry point
+ *
+ * @parent: function that inserts a node as the right-child of another node
+ * @value: function that inserts a node as the right-child of another node
+ *
+ * Return: Always 0 (Success)
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	if (parent->right != NULL)
+	{
+		newNode->right = parent->right;
+		parent->right->parent = newNode;
+	}
+
+	parent->right = newNode;
+
+	return (newNode);
+}
+
+/**
  * binary_tree_nodes - Entry point
  *
  * @tree: function that counts the nodes with at least 1 child in a binary tree
