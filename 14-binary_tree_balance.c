383fe09844686f71885cb7e425b62c8d603838d6
GraceKayem <11557@holbertonstudents.com>
2025-08-12 00:46:03 +1000

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
-
 /**
  * binary_tree_height - Measures the height of a binary tree
  *
@@ -13,16 +12,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
     size_t leftHeight = 0;
     size_t rightHeight = 0;
-
+    
     if (tree == NULL)
         return (0);
-
+    
     if (tree->left)
         leftHeight = 1 + binary_tree_height(tree->left);
-
+    
     if (tree->right)
         rightHeight = 1 + binary_tree_height(tree->right);
-
+    
     return (leftHeight > rightHeight ? leftHeight : rightHeight);
 }
 
@@ -31,23 +30,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
  *
  * @tree: Pointer to the root node of the tree to measure the balance factor
  *
- * Return: Balance factor (height of left subtree - height of right subtree),
- *         or 0 if tree is NULL
+ * Return: Balance factor (height of left subtree
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
     size_t leftHeight = 0;
     size_t rightHeight = 0;
-
+    
     if (tree == NULL)
         return (0);
-
+    
     if (tree->left)
         leftHeight = 1 + binary_tree_height(tree->left);
-
+    
     if (tree->right)
         rightHeight = 1 + binary_tree_height(tree->right);
-
+        
     return (int)(leftHeight - rightHeight);
 }
 
@@ -64,12 +62,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
     if (newNode == NULL)
         return (NULL);
-
+        
     newNode->n = value;
     newNode->parent = parent;
     newNode->left = NULL;
     newNode->right = NULL;
-
+        
     return (newNode);
 }
 
@@ -84,7 +82,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
     binary_tree_t *newNode;
-
+    
     if (parent == NULL)
         return (NULL);
 
