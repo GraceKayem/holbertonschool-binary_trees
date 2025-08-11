0498cac3b7e8df531134aa92fe5817d524cace07
GraceKayem <11557@holbertonstudents.com>
2025-08-11 13:11:35 +0000

    save

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -19,7 +19,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree->left == NULL && root->right == NULL)
 	{
-		return (0);
+		return (1);
 	}
 
 	if (tree->left != NULL && tree->right != NULL)
