9430d2507334be5429d9d7767c346dee5b88792c
GraceKayem <11557@holbertonstudents.com>
2025-08-11 13:33:49 +0000

    save

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,6 +3,34 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Entry point
+ *
+ * @tree: function that measures the height of a binary tree
+ *
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t leftHeight;
+	size_t rigthHeight;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+
+	leftHeight = binary_tree_height(tree->left);
+	rigthHeight = binary_tree_height(tree->right);
+
+	return ((leftHeight > rigthHeight ? leftHeight : rigthHeight) + 1);
+}
+
+/**
  * binary_tree_balance - Entry point
  *
  * @tree: function that measures the balance factor of a binary tree
