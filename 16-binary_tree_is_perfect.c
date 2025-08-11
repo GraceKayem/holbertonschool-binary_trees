2dd045962371a48619511b1d04127012ca3f7d6f
GraceKayem <11557@holbertonstudents.com>
2025-08-11 12:38:47 +0000

    save

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,7 +2,24 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_size - Entry point
+ *
+ * @tree: function that measures the size of a binary tree
+ *
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
 
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	return (binary_tree_size(tree->left) +
+		binary_tree_size(tree->right) + 1);
+}
 
 /**
  * binary_tree_is_perfect - Entry point
